ae996b6873b95bc86b5408c196765f0f2c805f31
morris <akunnemorris@gmail.com>
2024-10-05 04:23:58 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,28 @@ import {BrowserRoute, Navigate, Routes, Route} from 'react-router-dom';
 import Homepage from './scenes/homepage';
 import Loginpage from './scenes/loginpage';
 import Profilepage from './scenes/profilepage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import {createTheme, CssBaseline} from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { ThemeProvider } from '@emotion/react';
 
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  
   return (
     <div className="App">
       <BrowserRoute>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
       <Routes>
         <Route path='/' element={<Loginpage/>} />
         <Route path='/Homepage' element={<Homepage/>} />
         <Route path='/Profile/:userId' element={<Profilepage/>} />
       </Routes>
+      </ThemeProvider>
       </BrowserRoute>
     </div>
   );
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import {Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery} from '@mui/material';
+
+import {Search, Message, DarkMode, LightMode, Notification, Help, Menu, Close} from '@mui/icons-material';
+
+import { useDispatch, useSelector} from 'react-redux';
+import { setMode, setLogout } from '../../state';
+import FlexBetween from '../../components/flexBetween';
 
 const Navbar = () => {
   return (
@@ -6,4 +14,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar 
