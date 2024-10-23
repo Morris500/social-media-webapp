553b8ebd9293d577a83fc03f2e3e555ce9c6aa53
morris <akunnemorris@gmail.com>
2024-10-23 18:41:02 +0400

    form

--- client/src/scenes/loginpage/Form.jsx
+++ client/src/scenes/loginpage/Form.jsx
@@ -45,7 +45,7 @@ function Form (){
     const {palette} = useTheme();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const isNonMobile = useMediaQuery('(mkin-width:600px)');
+    const isNonMobile = useMediaQuery('(min-width:600px)');
     const isLogin = pageType === 'login';
     const isRegister = pageType === 'register';
 
@@ -54,7 +54,41 @@ function Form (){
     initialValues={isLogin ? initalValueLogin : initalValueRegister}
     validationSchema={isLogin ? loginSchema : registerSchema}
     >
-        {({values, errors, touched, handelchange, handelBlur, handelSubmit, setFieldValue, resetForm, }) => (<form onSubmit={handelSubmit})}
+        {({values, errors, touched, handelchange, handelBlur, handelSubmit, setFieldValue, resetForm, }) => (
+            <form onSubmit={handelSubmit}>
+                <Box display='grid' gap= '30px' gridTemplateColumns='repeat(4, minmax
+                (0, 1fr))'
+                sx={{'& > div': {gridColumn: isNonMobile ? undefined : 'span4'},
+                }}>
+                    {isRegister && (
+                        <>
+                        <TextField label='First Name' onBlur={handelBlur} onChange={handelchange} value={values.firstName} name='firstName' error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                        helperText={touched.firstName && errors.firstName} sx={{gridColumn: 'span 2'}} 
+                        />
+                        <TextField label='Last Name' onBlur={handelBlur} onChange={handelchange} value={values.lastName} name='lastName' error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                        helperText={touched.lastName && errors.lastName} sx={{gridColumn: 'span 2'}} 
+                        />
+                        <TextField label='Location' onBlur={handelBlur} onChange={handelchange} value={values.location} name='location' error={Boolean(touched.location) && Boolean(errors.location)}
+                        helperText={touched.location && errors.location} sx={{gridColumn: 'span 2'}} 
+                        />
+                        <TextField label='Occupation' onBlur={handelBlur} onChange={handelchange} value={values.occupation} name='occupation' error={Boolean(touched.occupation) && Boolean(errors.occupation)}
+                        helperText={touched.occupation && errors.occupation} sx={{gridColumn: 'span 2'}} 
+                        />
+                        <Box gridColumn='span 4' border={'1px solid ${palette.neutral.medium}'}
+                        borderRadius='5px' p='1rem'>
+                            <Dropzone acceptedFiles='.jpg,.jped,.png' multiple={false} onDrop={(acceptedFiles) => setFieldValue('picture', acceptedFiles[0])} >
+                                {({getRootProps, getInputProps}) => (
+                                    <Box {...getRootProps()} border={'2px dashed ${palette.primary.main}'} p='1rem' sx={{'&:hover': {cursor: 'pointer'}}}>
+                                        <input {...getInputProps()} />
+                                    </Box>
+                                )}
+                            </Dropzone>
+                        </Box>
+                        </>
+                    )}
+                </Box>
+            </form>
+    )}
 
         </formik> )
 }
