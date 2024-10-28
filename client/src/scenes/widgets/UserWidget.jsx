cfcb03726c15843f0efeab7284f7f8af54119f55
morris <akunnemorris@gmail.com>
2024-10-28 09:49:14 +0400

    UserWidget.jsx

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,57 @@
+import { ManageAccountsOutlined, EditOutlined, LocationOnOutlined, WorkOutlineOutlined } from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "../../components/UserImage";
+import FlexBetween from "../../components/flexBetween";
+import WidgetWrapper from "../../components/widgetwrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import state from "../../state";
+
+const UserWidget = ({userId, picturePath}) => {
+    const {user, setUser} = useState(null);
+    const {palette} = useTheme();
+    const navigate = useNavigate();
+    const token = useSelector((state) => state.token);
+    const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
+    const main = palette.neutral.main;
+
+    const getUser = async () => {
+        const response = await fetch(`http://localhost:3002/users/${userId}`, 
+            {
+                method: "GET",
+                hearders: {Authorization: `Bearer${token}`},
+             });
+             const data = await response.json();
+             setUser(data);
+    };
+    useEffect(() => {
+        getUser();
+    }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
+if (!user) {
+    return null;
+}
+
+const {firstName, lastName, location, occupation, viewedProfile, impression, friends,} = user;
+ return (
+    <WidgetWrapper>
+        {/* first row */}
+        <FlexBetween gap='0.5rem' pd='1.1rem' onClick={() => navigate(`/profile/${userId}`)}>
+            <FlexBetween gap='1rem'>
+                <UserImage image={picturePath} />
+            <Box>
+                <Typography variant='h4' color={dark} fontweight='500' sx={{'&:hover':{color: palette.primary.light,
+                    cursor: 'pionter'}}} >
+                        {firstName} {lastName}
+                    </Typography>
+                    <Typography  color={medium}>{friends.length} friends</Typography>
+            </Box>
+            <ManageAccountsOutlined/>
+           </FlexBetween>
+            
+     </FlexBetween>
+  </WidgetWrapper>
+ )
+};
