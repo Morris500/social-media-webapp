b266fe88c7ecb27570d4edea51880920ea5960bd
morris <akunnemorris@gmail.com>
2024-10-28 17:53:10 +0400

    userwiget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -50,6 +50,33 @@ const {firstName, lastName, location, occupation, viewedProfile, impression, fri
             </Box>
             <ManageAccountsOutlined/>
            </FlexBetween>
+           <Divider />
+
+           {/* second row */}
+           <Box p='1rem o'>
+            <Box display='flex' alignItems='center' gap='1rem' mb='0.5rem'>
+                <LocationOnOutlined fontSize="large" sx={{color: main}} />
+                <Typography color={medium}>{location}</Typography>
+            </Box>
+            <Box display='flex' alignItems='center' gap='1rem'>
+                <WorkOutlineOutlined fontSize="large" sx={{color:main}} />
+                <Typography color={medium}>{occupation}</Typography>
+            </Box>
+           </Box>
+
+           {/* third row */}
+           <Box p='1rem 0'>
+            <FlexBetween mb= '0.5rem'>
+                <Typography color={medium} >who's viewed your profile</Typography>
+                <Typography color={main} fontweight='500'>{viewedProfile}</Typography>
+            </FlexBetween>
+            <FlexBetween>
+                <Typography color={medium}>Impressions of your post</Typography>
+                <Typography color={main} fontWeight='500'>{impression}</Typography>
+            </FlexBetween>
+           </Box> 
+
+           {/*  */}
             
      </FlexBetween>
   </WidgetWrapper>
