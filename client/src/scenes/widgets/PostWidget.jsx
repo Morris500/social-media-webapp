df112616cb1b6b5b8b95d479400e1d1b04fb5316
morris <akunnemorris@gmail.com>
2024-11-07 02:19:16 +0100

    postwidget

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,4 +1,19 @@
 import { ChatBubbleOutlineOutlined, FavoriteBorderOutlined, FavoriteOutlined, ShareOutlined, } from "@mui/icons-material";
 import {Box, Divider, IconButton, Typography, useTheme} from "@mui/material";
 import FlexBetween from "../../components/flexBetween";
-import Friend 
+import Friend from "../../components/Friend";
+import WidgetWrapper from "../../components/widgetwrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "../../state";
+
+const PostWidget = ({postId, postUserId, name, description, location, picturePath, userPicturePath, likes, comments}) => {
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+
+    const {palette} = useTheme();
+    const primaryLight = palette.primary.light;
+    const primaryDark = palette.primary.dark;
+    const main = palette.neutral.main;
+    const meduim = palette.neutral.medium;
+}
